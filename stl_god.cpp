9716c05595fd30c7eadf54f273da861f6ce23b8b
akinbowale <akinbowale.akt@gmail.com>
2020-11-04 12:16:54 +0100

    using sets to solve an interval problem

--- stl_god.cpp
+++ stl_god.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <set>
+#include <map>
 #include <algorithm>
 
 using namespace std;
@@ -66,8 +68,93 @@ void vectorDemo() {
     cout << endl;
 }
 
+void setDemo() {
+    set<int> S;
+    S.insert(1);
+    S.insert(2);
+    S.insert(-1);
+    S.insert(-10);
+
+    for (int x: S) cout << x << " ";
+    cout << endl;
+    
+    // -10 -1 1 2
+    auto it = S.find(-1);
+
+    if (it == S.end()) {
+        cout << "not present\n";
+    } else {
+        cout << "present\n";
+        cout << *it << endl;
+    }
+
+    auto it2 = S.lower_bound(-1);
+    auto it3 = S.upper_bound(0);
+    cout << *it2 << " " << *it3 << endl;
+
+    auto it4 = S.upper_bound(2);
+    if (it4 == S.end()) {
+        cout << "oops! sorry cant find something like that \n";
+    }
+
+}
+
+void mapDemo() {
+    map<int, int> A;
+    A[1] = 100;
+    A[2] = -1;
+    A[1000000232] = 1;
+
+    map<char, int> cnt;
+    string x = "akinbowale akin-taylor";
+
+    for (char c: x) {
+        cnt[c]++;
+    }
+
+    cout << cnt['a'] << " " << cnt['o'] << endl;
+
+}
+
+void PowerOfStl() {
+    set< pair<int, int> > S; 
+
+    S.insert({{ 401, 450 }});
+    S.insert({{ 10, 20 }});
+    S.insert({{ 2, 3 }});
+    S.insert({{ 30, 400}});
+
+    // 2, 3
+    // 10, 20
+    // 30, 400
+    // 401, 450
+
+    int point = 2;
+
+    auto it = S.upper_bound({ point, INT_MAX });
+
+
+    if (it == S.begin()) {
+        cout << "the given point is not lying in any interval..\n";
+        return;
+    }
+    it--;
+    pair<int, int> current = *it;
+    if (current.first <= point && point <= current.second) {
+        cout << "yes its present: " << current.first << " " << current.second << endl;
+    } else {
+        cout << "the given point is not lying in any interval..\n";
+    }
+
+
+}
+
 int main() {
     // C++ STL
+    // setDemo();
+    // mapDemo();
+    PowerOfStl();
 
 
+    return 1;
 }
